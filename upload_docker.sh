03a1223cc41148ffd6ba93c347d4c5e1b5f80194
Farrukh Khalid <farrukhkhalid.it@outlook.com>
2022-02-24 08:46:09 +0500

    scripts
    run_kubernetes
    upload_docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="farrukhkhalid/ml-microservice"
 
 # Step 2:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+docker login -u "farrukhkhalid" -p "FRkkingot8"
+docker tag ml-microservice $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
